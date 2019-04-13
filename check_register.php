459ea59492c55179f56c024c1820e7f7ef801f8e
ibrahim <abukhalil2b@gmail.com>
2019-04-13 08:12:42 +0400

    register pages

--- check_register.php
+++ check_register.php
@@ -8,7 +8,14 @@ if(isset($_POST['phone'])){
 	$phone      = $_POST['phone'];
 	$password   = $_POST['password'];
 	$wilaya     = $_POST['wilaya'];
-	
+    
+    $sql_check = "SELECT * FROM `users` WHERE phone = $phone"
+    $result = $conn->query($sql_check);
+    if($result->num_rows==1){
+        header('Location:register_done.php');die();
+    }
+    
+    
     $sql= "INSERT INTO `users` (`id`, `first_name`, `last_name`, `phone`, `password`, `wilaya`)".
     " VALUES (NULL, '$first_name', '$last_name', '$phone', '$password', '$wilaya');";
 
