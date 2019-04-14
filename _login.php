be5602accac46d296b60af2888495814e044fc18
ibrahim <abukhalil2b@gmail.com>
2019-04-14 08:33:01 +0400

    styles

---
+++ _login.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <form method="POST" action="check_login.php">
+        <input id="phone" placeholder="الهاتف" class="form-control" name="phone" value="" required="" autofocus="">
+        <input id="password" type="password" placeholder="كلمة المرور" class="form-control" name="password" required="">
+        <button id="submit" type="submit" class="btn-login">
+            دخول
+        </button>
+    </form>
+    <h4>برعاية: مؤسسة الفلق لخدمات الحاسب الآلي</h4>
+</div>
---
+++ _register.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <form method="POST" action="check_register.php">
+        <input  placeholder="الاسم"  name="first_name"  required >
+        <input  placeholder="القبيلة"  name="last_name"  required >
+        <input  type="number" placeholder="الهاتف"  name="phone"  required >
+        <input  placeholder="كلمة المرور"  name="password" required>
+        <input  placeholder="الولاية"  name="wilaya" required>
+        <button  type="submit" class="btn-login">
+            تسجيل جديد
+        </button>
+    </form>
+    <h4>برعاية: مؤسسة الفلق لخدمات الحاسب الآلي</h4>
+</div>
